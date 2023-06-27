6e97748a3806444f06b8526a6d23629f20fe0e5b
kp4593 <88659584+kp4593@users.noreply.github.com>
2023-06-27 23:26:22 +0530

    Add files via upload

---
+++ sasta kbc.py
@@ -0,0 +1,51 @@
+n = input("Are you ready to play??(yes/no)")
+prize=0
+if(n=='yes'):
+    x = int(input('''QUESTION 1. World literacy day is observed on:
+    1. Sep 8
+    2. Nov 28
+    3. May 2
+    4. Sept 22'''))
+    if(x==1):
+        print("Sep 8 is correct answer")
+        prize = prize+5000
+        print("Till now, you've won", prize)
+        y = int(input('''QUESTION 2: The langauge of Lakshadweep, a Union Territory of India is:
+        1. Tamil
+        2. Hindi
+        3. Malayalam
+        4. Telugu'''))
+        if(y==3):
+            print("Malayalam is correct answer")
+            prize = prize+15000
+            print("Till now, you've won", prize)
+            z = int(input('''QUESTION 3: Bahubaliu festival is related to:
+            1. Islam
+            2. Hindiuism
+            3. Buddhism
+            4. Jainism'''))
+            if(z==4):
+                print("Jainism is the correct answer!!")
+                prize = prize+80000
+                print("Till now, you've won", prize)
+                a = int(input('''QUESTION 4. Who is the author of Manas Ka Hans:
+                1. Kushwant Singh
+                2. Prem Chand
+                3. Jayshankar Prasad
+                4. Amrit Lal Nagar'''))
+                if(a==4):
+                    print("Amrit Lal Nagar is the correct answer!!")
+                    print("5 CRORE")
+                    prize= 50000000
+                    print("Congratulations, you've won", prize)
+                else:
+                    print("Sorry, wrong answer, you've lost all your winnings")
+
+            else:
+                print("Wrong answer, you've bveen eliminated")
+
+        else:
+            print("Wrong answer, you've been eliminated")
+
+    else:
+        print("wrong answer, you're eliminated")
